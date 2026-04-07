67a9c700b7d5d626651b9f4dbd83cd47e6b7aa02
Nicolas Hoffmann <hoffmannni92690@th-nuernberg.de>
2026-04-07 21:10:17 +0200

    migrate capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.bowling',
+  appId: 'de.lightningbowl',
   appName: 'Lightning Bowl',
   webDir: 'www',
   server: {
